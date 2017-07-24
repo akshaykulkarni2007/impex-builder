597ae667cafffba980129bcda5c37017b6a50254
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2017-07-25 00:34:16 +0530

    basic add row-col, primart css, output box

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,23 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   //change event to body change
-  var tr = '<tr><td><input type="text" class="ip"></td><td><a href="#" class="add-col">+</a></td></tr>'
+  var tr = $('#main-table tbody tr').eq(1).html();
   var td = '<td><input type="text" class="ip"></td>';
   var rowCount = $('#main-table tbody tr').length;
+  var colCount = $('#main-table tbody tr:first td').length - 1;
   var index = 0;
 
-  $("#main-table tbody tr").each(function(){
+  $("#main-table tbody tr").each(function() {
     $(this).attr("id", "row-id-"+ ++index);
   });
 
   $(".add-col").parent().attr("rowspan", rowCount);
 
-  $(".add-col").click(function(){
-    //console.log($("#main-table tr").eq($(this).closest("tr")[0].rowIndex).html())
+  $(".add-col").click(function(e) {
+    e.preventDefault();
     $("#main-table tr").find("td:last").prev().after(td);
+    colCount++;
+    tr = $('#main-table tbody tr').eq(1).html();
   });
 
-  $("#add-row").click(function(){
-    $('#main-table tr:last').prev().after(tr);
+  $(".add-row").click(function(e) {
+    e.preventDefault();
+    $('#main-table tbody tr:last').after("<tr>" + tr + "</tr>");
+    rowCount++;
+    $(".add-col").parent().attr("rowspan", rowCount);
   });
-})
+
+});
