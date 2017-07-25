398a8d423254d5091f03003a82c6e12e09012853
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2017-07-25 20:22:03 +0530

    added header, delete buttons at right places

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 
   //change event to body change
   var tr = $('#main-table tbody tr').eq(1).html();
-  var td = '<td><input type="text" class="ip"></td>';
+  var td_header = '<td><label contenteditable="true"></td>';
+  var td_body = '<td><input type="text" class="ip"><a href="#" class="table-button delete-row btn btn-block btn-danger" title="Delete Row">-</a></td>';
   var rowCount = $('#main-table tbody tr').length;
   var colCount = $('#main-table tbody tr:first td').length - 1;
   var index = 0;
@@ -11,11 +12,14 @@ $(document).ready(function() {
     $(this).attr("id", "row-id-"+ ++index);
   });
 
-  $(".add-col").parent().attr("rowspan", rowCount);
+  $(".table-button.add-column").parent().attr("rowspan", rowCount);
 
-  $(".add-col").click(function(e) {
+  $(".add-column").click(function(e) {
     e.preventDefault();
-    $("#main-table tr").find("td:last").prev().after(td);
+    $(".table-button.delete-row").remove();
+
+    $("#main-table thead tr").find("td:last").prev().after(td_header);
+    $("#main-table tbody tr").find("td:last").prev().after(td_body);
     colCount++;
     tr = $('#main-table tbody tr').eq(1).html();
   });
@@ -24,7 +28,7 @@ $(document).ready(function() {
     e.preventDefault();
     $('#main-table tbody tr:last').after("<tr>" + tr + "</tr>");
     rowCount++;
-    $(".add-col").parent().attr("rowspan", rowCount);
+    $(".table-button").parent().attr("rowspan", rowCount);
   });
 
 });
