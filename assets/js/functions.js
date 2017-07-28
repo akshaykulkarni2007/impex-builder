f653d8d7915806a633cdf4ed2281e1ac54a2813c
yogesh <yogesh.patki@gmail.com>
2017-07-28 22:18:44 +0530

    jquery implementation using event based data binding

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,30 +1,79 @@
+var tableSource,
+    outSource,
+    tableTemplate,
+    outTemplate,
+    impex_data;
+
 $(document).ready(function() {
+  tableSource =  $("#impex-table").html();
+  outSource =    $("#impex-out").html();
+  tableTemplate  = Handlebars.compile(tableSource);
+  outTemplate    = Handlebars.compile(outSource);
+  impex_data = [['','','',''],['','','',''],['','','','']];
+  printTable(impex_data);
+  printImpexOutput(impex_data);
+  
 
-  //change event to body change
-  var tr = $('#main-table tbody tr').eq(1).html();
-  var td = '<td><input type="text" class="ip"></td>';
-  var rowCount = $('#main-table tbody tr').length;
-  var colCount = $('#main-table tbody tr:first td').length - 1;
-  var index = 0;
-
-  $("#main-table tbody tr").each(function() {
-    $(this).attr("id", "row-id-"+ ++index);
+  $(document).on('input','input',function(){
+    var e = $.Event("impex-updated");
+    e.row = $(this).data('row');
+    e.col = $(this).data('col');
+    e.val = $(this).val();
+    $(document).trigger(e);
   });
 
-  $(".add-col").parent().attr("rowspan", rowCount);
-
-  $(".add-col").click(function(e) {
-    e.preventDefault();
-    $("#main-table tr").find("td:last").prev().after(td);
-    colCount++;
-    tr = $('#main-table tbody tr').eq(1).html();
+  $(document).on('click','.removeRow',function(){
+    var row = $(this).data('row');
+    impex_data.splice(row,1);
+    $(document).trigger('content-refresh');
   });
 
-  $(".add-row").click(function(e) {
-    e.preventDefault();
-    $('#main-table tbody tr:last').after("<tr>" + tr + "</tr>");
-    rowCount++;
-    $(".add-col").parent().attr("rowspan", rowCount);
+  $(document).on('click','.removeColumn',function(){
+    var col = $(this).data('col');
+    for(var row of impex_data){
+      row.splice(col,1);
+    }
+    $(document).trigger('content-refresh');
   });
 
+  $('.addColumn').on('click',function(){
+    for(var row of impex_data){
+      row.push('');
+    }
+    $(document).trigger('content-refresh');
+  });
+
+  $('.addRow').on('click',function(){
+    var count = $(this).data('count');
+    if(impex_data.length > 0){
+      for(var i=0;i<count;i++){
+        var row = [] 
+        for(var j = 0; j <impex_data[0].length;j++){
+          row.push('');
+        }
+        impex_data.push(row);
+      }
+      $(document).trigger('content-refresh');
+    }
+  });
 });
+
+
+$(document).on('impex-updated',function(e){
+    impex_data[e.row][e.col] = e.val;
+    printImpexOutput(impex_data);
+});
+
+$(document).on('content-refresh',function(){
+    printTable(impex_data);
+    printImpexOutput(impex_data);
+});
+
+function printImpexOutput(impex_data) {
+  $('.impex-output').html(outTemplate({rows:impex_data}));
+}
+
+function printTable(impex_data){
+  var html = tableTemplate({rows: impex_data});
+  $('table').html(html);
+}
