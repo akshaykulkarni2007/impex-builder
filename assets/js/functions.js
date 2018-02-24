2c34fe9db1b9c87c470e71de59c327d40cfc2c4b
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2018-02-24 20:51:15 +0530

    Phase-I done

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,15 +4,18 @@ var tableSource,
     outTemplate,
     impex_data,
     headers_data,
-    operation;
+    operation,
+    macro;
 
 $(document).ready(function() {
+
   tableSource =  $("#impex-table").html();
   outSource =    $("#impex-out").html();
   tableTemplate  = Handlebars.compile(tableSource);
   outTemplate    = Handlebars.compile(outSource);
   impex_data = [['','','',''],['','','',''],['','','','']];
   headers_data = populateHeadersData(impex_data);
+  macro = $(".macro").val();
   operation = $('[name="operation"]').val() + " " + $("#structure-name").val() + ";";
 
   new Clipboard('.clipboard');
@@ -20,6 +23,7 @@ $(document).ready(function() {
   $(".modal-content").slimScroll ({
     height: "90vh"
   });
+
   $("#impex-output-body").slimScroll ({
     height: "300px"
   });
@@ -43,73 +47,105 @@ $(document).ready(function() {
 
   $("label.table-header").css("max-width", $("#main-table tbody tr td").css("width"));
 
+  $(".macro").on('keyup paste',function() {
+    macro = $(this).val();
+    printImpexOutput(impex_data, headers_data);
+  });
+
   $('[name="operation"]').on('change',function() {
     operation = $(this).val() + " " + $("#structure-name").val() + ";";
     printImpexOutput(impex_data, headers_data);
   });
+
   $("#structure-name").on('input',function() {
     operation = $('[name="operation"]:checked').val() + " " + $(this).val() + ";";
     printImpexOutput(impex_data, headers_data);
   });
+
   $(document).on('input','input.impex-input',function() {
     var e = $.Event("impex-updated");
     e.row = $(this).data('row');
     e.col = $(this).data('col');
     e.val = $(this).val();
+
     $(document).trigger(e);
   });
 
   $(document).on('click','.removeRow',function() {
     var row = $(this).data('row');
-    impex_data.splice(row,1);
-    $(document).trigger('content-refresh');
+
+    if(row) {
+      impex_data.splice(row,1);
+
+      $(document).trigger('content-refresh');
+    }
+    
   });
 
   $(document).on('click','.removeColumn',function() {
     var col = $(this).data('col');
-    for(var row of impex_data) {
-      row.splice(col,1);
+
+    if(col) {
+      for(var row of impex_data) {
+        row.splice(col,1);
+      }
+  
+      headers_data.splice(col,1);
+      updateHeaderIndices(headers_data);
+  
+      $(document).trigger('content-refresh');
     }
-    headers_data.splice(col,1);
-    updateHeaderIndices(headers_data);
-    $(document).trigger('content-refresh');
+    
   });
 
   $('.addColumn').on('click',function() {
-    for(var row of impex_data) {
-      row.push('');
+    if($(this).data("allow") == "allow") {
+      for(var row of impex_data) {
+        row.push('');
+      }
+  
+      headers_data.push('Header - ' + (impex_data[0].length));
+      $(document).trigger('content-refresh');
+  
+      removeLastSemiColon();
     }
-    // $('.main-table-container').scrollLeft($(".main-table-container").width());
-    headers_data.push('Header - ' + (impex_data[0].length));
-    $(document).trigger('content-refresh');
-    removeLastSemiColon();
   });
 
   $('.addRow').on('click',function() {
     var count = $(this).data('count');
-    if(impex_data.length > 0) {
-      for(var i=0;i<count;i++) {
-        var row = []
-        for(var j = 0; j <impex_data[0].length;j++) {
-          row.push('');
+
+    if(count) {
+      if(impex_data.length > 0) {
+        for(var i=0;i<count;i++) {
+          var row = []
+          for(var j = 0; j <impex_data[0].length;j++) {
+            row.push('');
+          }
+          impex_data.push(row);
         }
-        impex_data.push(row);
+  
+        $(document).trigger('content-refresh');
       }
-      $(document).trigger('content-refresh');
     }
+    
   });
 
   $('#download-button').click(function() {
     if($("#file-name").val()) {
-      var file_content = $(".impex-output-body").html().trim();
-      file_content = file_content.replace(/(<([^>]+)>)/ig,"");
-      file_content = file_content.replace("&amp;", "&");
+
+      var file_content = $(".impex-output-body").html();
       var file_name = $("#file-name").val() + '.impex';
       var link = document.createElement('a');
       var mimeType = "text/plain";
+
+      file_content = file_content.replace(/<br\s*[\/]?>/gi, "\n");
+      file_content = file_content.replace(/(<([^>]+)>)/ig,"");
+      file_content = file_content.replace("&amp;", "&");
+      
       link.setAttribute('download', file_name);
       link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(file_content));
       link.click();
+
     } else {
       alert("Please Enter File Name");
     }
@@ -117,7 +153,8 @@ $(document).ready(function() {
 
   $(".accordion-trigger").click(function(){
     $(this).next(".accordion-content").toggleClass("active");
-  })
+  });
+
 
 });
 
@@ -125,14 +162,17 @@ $(document).on('impex-updated',function(e) {
   if(!e.header_data_updated){
     impex_data[e.row][e.col] = e.val;
   }
-    printImpexOutput(impex_data,headers_data);
+  
+  printImpexOutput(impex_data,headers_data);
+
 });
 
 $(document).on('content-refresh',function() {
-  if(impex_data.length === 0 || impex_data[0].length ===0) {
+  if(impex_data.length === 0 || impex_data[0].length === 0) {
       impex_data = [['']];
       headers_data = ["Header - 1"];
   }
+
   printTable(impex_data, headers_data);
   firstColumnWidth();
   printImpexOutput(impex_data,headers_data);
@@ -140,8 +180,10 @@ $(document).on('content-refresh',function() {
 
 function printImpexOutput(impex_data,headers_data) {
   $('#impex-output-body').html(outTemplate({rows:impex_data,
-                                            headers:headers_data,
+                                            macro: macro,
+                                            headers: headers_data,
                                             operation: operation}));
+  $(".output-macro").html(macro.replace(/\r?\n/g,'<br/>'));
 }
 
 function printTable(impex_data,headers_data) {
@@ -149,6 +191,7 @@ function printTable(impex_data,headers_data) {
                               headers: headers_data
                           });
   $('table').html(html);
+
   bindEvenets();
 }
 
@@ -160,10 +203,12 @@ function bindEvenets() {
       if(val==="Header - "+($(this).data("col")+1))
         $(this).html("");
     });
+
     $(this).focusout(function() {
       if($(this).html().trim() == "") {
         $(this).html("Header - " + ($(this).data('col')+1));
       }
+
       headers_data[$(this).data('col')] = $(this).html();
     });
   });
@@ -171,8 +216,11 @@ function bindEvenets() {
   $('label.table-header').each(function() {
     $(this).on('keyup',function() {
       headers_data[$(this).data('col')] = $(this).html();
+
       var e = $.Event("impex-updated");
+
       e.header_data_updated = true;
+
       $(document).trigger(e);
     });
   });
@@ -180,20 +228,24 @@ function bindEvenets() {
   $(".impex-header").focusin(function() {
     if($(this).html() === "Reference Header")
       $(this).html("");
+
     $(this).focusout(function() {
       if($(this).html().trim() == "") {
         $(this).html("Reference Header");
       }
     });
+
   });
 }
 
 function populateHeadersData(rows) {
   var row = rows[0];
   var header_data = [];
+
   for(var i = 0; i < row.length; i++) {
     header_data.push('Header - ' + (i+1));
   }
+
   return header_data;
 }
 
@@ -209,6 +261,7 @@ function firstColumnWidth() {
   $("#main-table tbody tr").each(function() {
     $(this).find("td:first").addClass("row-number")
   });
+
   var labelWidth = $("#main-table label.table-header:last").outerWidth();
   var pseudoWidth = parseInt(window.getComputedStyle(document.querySelector(".row-number"), ':before').width);
   var firstColWidth = $("#main-table tbody tr:first td:first").outerWidth() + pseudoWidth + 10;
@@ -217,6 +270,7 @@ function firstColumnWidth() {
     "float": "right",
     "width": labelWidth
   });
+
   if($("#main-table").width() >= ($(window).width() -50 )) {
     $("#main-table tbody tr").each(function() {
       $(this).find("td:first").css({
